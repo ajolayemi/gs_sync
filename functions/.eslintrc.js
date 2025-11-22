66db788a3ef5d22f30eb0f01ca313445c5927126
Kehinde Jolayemi <ajolayemi82@gmail.com>
2025-11-22 17:06:50 +0100

    added firebase, gitignore and lint file

---
+++ functions/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+    "/generated/**/*", // Ignore generated files.
+  ],
+  plugins: ["@typescript-eslint", "import"],
+  rules: {
+    "quotes": ["error", "double"],
+    "import/no-unresolved": 0,
+    "indent": ["error", 2],
+    "linebreak-style": 0,
+    "operator-linebreak": 0,
+    "max-len": [
+      "error",
+      {
+        code: 110,
+        comments: 120,
+        ignorePattern: "^\\s*import|^\\s*export|^\\s*return|^\\s*logger",
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
